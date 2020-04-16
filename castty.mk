d0481a35f0c922503f4a2c60fd83cedeb3717339
Neil T. Dantam <ndantam@mines.edu>
2020-04-16 03:56:35 -0600

    Initial commit

---
+++ castty.mk
@@ -0,0 +1,11 @@
+src/%.mkv: %.ogv
+	mkdir -p src
+	ffmpeg -i "$<" \
+	 -codec:v libx264 -crf 0 \
+	-codec:a copy \
+	-max_muxing_queue_size 4096 \
+	"$@"
+
+# Notes:
+# - lossless libx264 was smaller than libvpx-vp9
+#   -codec:v libvpx-vp9 -lossless 1 \
